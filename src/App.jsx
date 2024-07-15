49c4dcdc7e2250ee8c364f038267d071ec299cb8
JatinKumar65042 <jatinsokhal2300@gmail.com>
2024-07-15 11:26:57 +0530

    Cleaned aboutus added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Footer from './Components/Footer'
 import HomePage from './Pages/HomePage'
 import HomeLayout from './Layouts/HomeLayout'
+import AboutUs from './Pages/AboutUs'
+import NotFound from './Pages/NotFound';
 
 function App() {
   
 
   return (
     <>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<HomePage/>}></Route>
-      </Routes> */}
+        <Route path="/about" element={<AboutUs />} ></Route>
+
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
       {/* <Footer/> */}
       {/* <HomeLayout/> */}
-      <HomePage/>
+      {/* <HomePage/> */}
     </>
   )
 }
