19a77695bbbabd52509c2d04cf832f470f03c519
JatinKumar65042 <jatinsokhal2300@gmail.com>
2024-08-21 22:45:04 +0530

    Added contact, denied, course desc

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import Footer from './Components/Footer'
 import HomePage from './Pages/HomePage'
 import HomeLayout from './Layouts/HomeLayout'
 import AboutUs from './Pages/AboutUs'
+import Denied from './Pages/Denied';
+import Contact from './Pages/Contact';
 import NotFound from './Pages/NotFound';
+import CourseDescription from './Pages/Course/CourseDescription';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup'
 import CourseList from './Pages/Course/CourseList';
@@ -19,6 +22,9 @@ function App() {
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/about" element={<AboutUs />} ></Route>
         <Route path="/courses" element={<CourseList />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/denied" element={<Denied />} />
+        <Route path="/course/description" element={<CourseDescription />} />
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/login" element={<Login />} />
 
